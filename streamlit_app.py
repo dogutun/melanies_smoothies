c4e1bfea38936c2f00e8b1b6ac2ba4f4731a1789
dogutun <62527113+dogutun@users.noreply.github.com>
2025-06-23 09:57:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -40,3 +41,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
+smoothiesfroot_response=request("https://smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
